0f6d057b7fe18d51f95725346c0f575c0fd70165
Jason <xjasonlyu@gmail.com>
2020-10-09 15:57:45 +0800

    add comments

--- address.go
+++ address.go
@@ -5,43 +5,43 @@ import (
 )
 
 type Network struct {
-	InterfaceIndex    uint32
-	SubInterfaceIndex uint32
+	InterfaceIndex    uint32 /* Packet's interface index. */
+	SubInterfaceIndex uint32 /* Packet's sub-interface index. */
 	_                 [7]uint64
 }
 
 type Socket struct {
-	EndpointID       uint64
-	ParentEndpointID uint64
-	ProcessID        uint32
-	LocalAddress     [16]uint8
-	RemoteAddress    [16]uint8
-	LocalPort        uint16
-	RemotePort       uint16
-	Protocol         uint8
+	EndpointID       uint64    /* Endpoint ID. */
+	ParentEndpointID uint64    /* Parent endpoint ID. */
+	ProcessID        uint32    /* Process ID. */
+	LocalAddress     [16]uint8 /* Local address. */
+	RemoteAddress    [16]uint8 /* Remote address. */
+	LocalPort        uint16    /* Local port. */
+	RemotePort       uint16    /* Remote port. */
+	Protocol         uint8     /* Protocol. */
 	_                [3]uint8
 	_                uint32
 }
 
 type Flow struct {
-	EndpointID       uint64
-	ParentEndpointID uint64
-	ProcessID        uint32
-	LocalAddress     [16]uint8
-	RemoteAddress    [16]uint8
-	LocalPort        uint16
-	RemotePort       uint16
-	Protocol         uint8
+	EndpointID       uint64    /* Endpoint ID. */
+	ParentEndpointID uint64    /* Parent endpoint ID. */
+	ProcessID        uint32    /* Process ID. */
+	LocalAddress     [16]uint8 /* Local address. */
+	RemoteAddress    [16]uint8 /* Remote address. */
+	LocalPort        uint16    /* Local port. */
+	RemotePort       uint16    /* Remote port. */
+	Protocol         uint8     /* Protocol. */
 	_                [3]uint8
 	_                uint32
 }
 
 type Reflect struct {
-	TimeStamp int64
-	ProcessID uint32
-	layer     uint32
-	Flags     uint64
-	Priority  int16
+	TimeStamp int64  /* Handle open time. */
+	ProcessID uint32 /* Handle process ID. */
+	layer     uint32 /* Handle layer. */
+	Flags     uint64 /* Handle flags. */
+	Priority  int16  /* Handle priority. */
 	_         int16
 	_         int32
 	_         [4]uint64
@@ -52,13 +52,27 @@ func (r *Reflect) Layer() Layer {
 }
 
 type Address struct {
-	Timestamp int64
-	layer     uint8
-	event     uint8
-	Flags     uint8
-	_         uint8
-	length    uint32
-	union     [64]uint8
+	Timestamp int64 /* Packet's timestamp. */
+	layer     uint8 /* Packet's layer. */
+	event     uint8 /* Packet event. */
+
+	Flags uint8
+	/* Packet was sniffed? */
+	/* Packet is outbound? */
+	/* Packet is loopback? */
+	/* Packet is impostor? */
+	/* Packet is IPv6? */
+	/* Packet has valid IPv4 checksum? */
+	/* Packet has valid TCP checksum? */
+	/* Packet has valid UDP checksum? */
+
+	_      uint8     /* Reserved1. */
+	length uint32    /* Reserved2. */
+	union  [64]uint8 /* Reserved3. */
+	/* Network layer data. */
+	/* Flow layer data. */
+	/* Socket layer data. */
+	/* Reflect layer data. */
 }
 
 func (a *Address) Layer() Layer {
--- const.go
+++ const.go
@@ -1,13 +1,14 @@
 package divert
 
+//WinDivert layers.
 type Layer int
 
 const (
-	LayerNetwork Layer = iota
-	LayerNetworkForward
-	LayerFlow
-	LayerSocket
-	LayerReflect
+	LayerNetwork        Layer = iota /* Network layer. */
+	LayerNetworkForward              /* Network layer (forwarded packets) */
+	LayerFlow                        /* Flow layer. */
+	LayerSocket                      /* Socket layer. */
+	LayerReflect                     /* Reflect layer. */
 )
 
 func (l Layer) String() string {
@@ -27,19 +28,20 @@ func (l Layer) String() string {
 	}
 }
 
+// WinDivert events.
 type Event int
 
 const (
-	EventNetworkPacket Event = iota
-	EventFlowEstablished
-	EventFlowDeleted
-	EventSocketBind
-	EventSocketConnect
-	EventSocketListen
-	EventSocketAccept
-	EventSocketClose
-	EventReflectOpen
-	EventReflectClose
+	EventNetworkPacket   Event = iota /* Network packet. */
+	EventFlowEstablished              /* Flow established. */
+	EventFlowDeleted                  /* Flow deleted. */
+	EventSocketBind                   /* Socket bind. */
+	EventSocketConnect                /* Socket connect. */
+	EventSocketListen                 /* Socket listen. */
+	EventSocketAccept                 /* Socket accept. */
+	EventSocketClose                  /* Socket close. */
+	EventReflectOpen                  /* WinDivert handle opened. */
+	EventReflectClose                 /* WinDivert handle closed. */
 )
 
 func (e Event) String() string {
@@ -69,12 +71,13 @@ func (e Event) String() string {
 	}
 }
 
+// WinDivert shutdown parameter.
 type Shutdown int
 
 const (
-	ShutdownRecv Shutdown = iota
-	ShutdownSend
-	ShutdownBoth
+	ShutdownRecv Shutdown = iota /* Shutdown recv. */
+	ShutdownSend                 /* Shutdown send. */
+	ShutdownBoth                 /* Shutdown recv and send. */
 )
 
 func (h Shutdown) String() string {
@@ -90,14 +93,15 @@ func (h Shutdown) String() string {
 	}
 }
 
+// WinDivert parameters.
 type Param int
 
 const (
-	QueueLength Param = iota
-	QueueTime
-	QueueSize
-	VersionMajor
-	VersionMinor
+	QueueLength  Param = iota /* Packet queue length. */
+	QueueTime                 /* Packet queue time. */
+	QueueSize                 /* Packet queue size. */
+	VersionMajor              /* Driver version (major). */
+	VersionMinor              /* Driver version (minor). */
 )
 
 func (p Param) String() string {
@@ -117,29 +121,35 @@ func (p Param) String() string {
 	}
 }
 
+// WinDivert flags.
 const (
 	FlagDefault   = 0x0000
 	FlagSniff     = 0x0001
 	FlagDrop      = 0x0002
 	FlagRecvOnly  = 0x0004
+	FlagReadOnly  = FlagRecvOnly
 	FlagSendOnly  = 0x0008
+	FlagWriteOnly = FlagSendOnly
 	FlagNoInstall = 0x0010
 	FlagFragments = 0x0020
 )
 
+// WinDivert constants.
 const (
 	PriorityDefault    = 0
 	PriorityHighest    = 3000
-	PriorityLowest     = -3000
+	PriorityLowest     = -PriorityHighest
 	QueueLengthDefault = 4096
 	QueueLengthMin     = 32
 	QueueLengthMax     = 16384
-	QueueTimeDefault   = 2000
-	QueueTimeMin       = 100
-	QueueTimeMax       = 16000
-	QueueSizeDefault   = 4194304
-	QueueSizeMin       = 65535
-	QueueSizeMax       = 33554432
+	QueueTimeDefault   = 2000     /* 2s */
+	QueueTimeMin       = 100      /* 100ms */
+	QueueTimeMax       = 16000    /* 16s */
+	QueueSizeDefault   = 4194304  /* 4MB */
+	QueueSizeMin       = 65535    /* 64KB */
+	QueueSizeMax       = 33554432 /* 32MB */
+	BatchMax           = 0xff     /* 255 */
+	MTUMax             = 40 + 0xffff
 )
 
 const (
@@ -149,9 +159,4 @@ const (
 	NoICMPV6Checksum = 4
 	NoTCPChecksum    = 8
 	NoUDPChecksum    = 16
-)
-
-const (
-	BatchMax = 0xff
-	MTUMax   = 40 + 0xffff
 )
